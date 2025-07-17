4018279c700b29720b41511414978d8c50d9886c
jockopw <jockj9963@gmail.com>
2025-07-17 17:26:38 +0400

    Update JocksHubBETA.lua

--- JocksHubBETA.lua
+++ JocksHubBETA.lua
@@ -284,6 +284,25 @@ loadstring(game:HttpGet('https://raw.githubusercontent.com/ttwizz/Open-Aimbot/re
    end,
 })
 
+local Button = UniversalAimbotandEspTab:CreateButton({
+   Name = "Sense Esp",
+   Callback = function()
+-- 1. Load the library
+local Sense = loadstring(game:HttpGet('https://sirius.menu/sense'))()
+
+-- 2. Change the configuration.
+Sense.teamSettings.enemy.enabled = true
+Sense.teamSettings.enemy.box = true
+Sense.teamSettings.enemy.boxColor[1] = Color3.new(0, 0.25, 0.75)
+
+-- 3. Load the esp. It doesn't really matter where you put this, but it's recommended you put it at the end of your script.
+Sense.Load()
+
+-- 4. Unload the esp. When you unload Sense, it will clean up every drawing object and instance it has made.
+--Sense.Unload()
+   end,
+})
+
 local UniversalHitboxExpanderTab = Window:CreateTab("📦Universal Hitbox Expander📦", nil) -- Title, Image
 local UniversalHitboxExpanderSection = UniversalHitboxExpanderTab:CreateSection("May Not Work In All Games")
 
